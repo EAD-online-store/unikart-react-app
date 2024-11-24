52438de526de81ff49fcadfb1f8e4ce461d73071
NimeshRukshan <fernandownr.21@uom.lk>
2024-11-24 17:19:28 +0530

    Update DisplayPriceInRupees function to format prices in LKR with custom prefix

--- src/utils/DisplayPriceInRupees.js
+++ src/utils/DisplayPriceInRupees.js
@@ -1,6 +1,15 @@
-export const DisplayPriceInRupees = (price)=>{
-    return new Intl.NumberFormat('en-IN',{
-        style : 'currency',
-        currency : 'INR'
-    }).format(price)
-}
+export const DisplayPriceInRupees = (price) => {
+  const formattedPrice = new Intl.NumberFormat("si-LK", {
+    style: "currency",
+    currency: "LKR",
+  }).format(price);
+
+  return formattedPrice.replace(/^.*?(\d)/, "Rs. $1");
+};
+
+// export const DisplayPriceInRupees = (price) => {
+//   return new Intl.NumberFormat("si-LK", {
+//     style: "currency",
+//     currency: "LKR",
+//   }).format(price);
+// };
